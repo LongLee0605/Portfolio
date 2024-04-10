5965e3cc615e6cc22ac4051e907c2ec918083494
Le Tran Dang Long <longle@gleadsvn.local>
2024-04-10 17:38:15 +0700

    update mobile

--- app/layout.js
+++ app/layout.js
@@ -18,9 +18,9 @@ export default function RootLayout({ ...props }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <section className="flex mx-auto max-w-[1920px] w-full relative">
+        <section className="flex mx-auto max-w-[1920px] w-full relative flex-col lg:flex-row">
           {/* Left */}
-          <div className="w-1/4 mx-10 flex-col flex gap-6 fixed left-0 top-0 h-[100vh] justify-center">
+          <div className="lg:w-1/4 w-full p-10 mx-0 lg:mx-10 flex-col flex gap-6 relative lg:fixed left-0 top-0 justify-center">
             <HeroBgAnimation />
             <div className="flex justify-center mt-14">
               <img
@@ -62,10 +62,10 @@ export default function RootLayout({ ...props }) {
             </div>
           </div>
           {/* Right */}
-          <div className="w-3/4 ml-[30%] relative">
+          <div className="w-full lg:w-3/4 mx-0 lg:mx-5 lg:ml-[30%] relative">
             <div
               id="skills"
-              className="px-5 py-10 text-center flex flex-col items-center justify-center]"
+              className="px-5 py-10 text-center flex flex-col items-center justify-center"
             >
               <p
                 className="
@@ -80,8 +80,8 @@ export default function RootLayout({ ...props }) {
                 Here are some of my skills on which I have been working on for
                 the past 2 years.
               </p>
-              <div className="flex gap-10 justify-between ">
-                <div className="shadow-[0_0_12px_4px_rgba(133,_76,_230,_0.2)] hover:-translate-y-3 duration-200 hover:shadow-[0_15px_30px_rgba(133,_76,_230,_0.4)] px-4 pt-4 pb-8 rounded-3xl border-2 border-[#844ce696] w-1/2 ">
+              <div className="flex gap-10 justify-center lg:justify-between flex-wrap lg:flex-nowrap">
+                <div className="shadow-[0_0_12px_4px_rgba(133,_76,_230,_0.2)] hover:-translate-y-3 duration-200 hover:shadow-[0_15px_30px_rgba(133,_76,_230,_0.4)] px-4 pt-4 pb-8 rounded-3xl border-2 border-[#844ce696] w-full lg:w-1/2 ">
                   <p className="text-xl font-bold text-center pb-4">FrontEnd</p>
                   <div className="flex gap-3 flex-wrap justify-center">
                     {Skills?.frontend.map((item) => {
@@ -97,7 +97,7 @@ export default function RootLayout({ ...props }) {
                     })}
                   </div>
                 </div>
-                <div className="shadow-[0_0_12px_4px_rgba(133,_76,_230,_0.2)] hover:-translate-y-3 duration-200 hover:shadow-[0_15px_30px_rgba(133,_76,_230,_0.4)] px-4 pt-4 pb-8 rounded-3xl border-2 border-[#844ce696] w-1/2 ">
+                <div className="shadow-[0_0_12px_4px_rgba(133,_76,_230,_0.2)] hover:-translate-y-3 duration-200 hover:shadow-[0_15px_30px_rgba(133,_76,_230,_0.4)] px-4 pt-4 pb-8 rounded-3xl border-2 border-[#844ce696] w-ful lg:w-1/2 ">
                   <p className="text-xl font-bold text-center pb-4">BackEnd</p>
                   <div className="flex gap-3 flex-wrap justify-center">
                     {Skills?.backend.map((item) => {
@@ -114,7 +114,7 @@ export default function RootLayout({ ...props }) {
                   </div>
                 </div>
               </div>
-              <div className="mt-10 shadow-[0_0_12px_4px_rgba(133,_76,_230,_0.2)] hover:-translate-y-3 duration-200 hover:shadow-[0_15px_30px_rgba(133,_76,_230,_0.4)] px-4 pt-4 pb-8 rounded-3xl border-2 border-[#844ce696] w-1/2 ">
+              <div className="mt-10 shadow-[0_0_12px_4px_rgba(133,_76,_230,_0.2)] hover:-translate-y-3 duration-200 hover:shadow-[0_15px_30px_rgba(133,_76,_230,_0.4)] px-4 pt-4 pb-8 rounded-3xl border-2 border-[#844ce696] w-full lg:w-1/2 ">
                 <p className="text-xl font-bold text-center pb-4">Orther</p>
                 <div className="flex gap-3 flex-wrap justify-center">
                   {Skills?.orther.map((item) => {
@@ -131,7 +131,7 @@ export default function RootLayout({ ...props }) {
                 </div>
               </div>
             </div>
-            <div id="experience" className="text-center px-5 py-10">
+            <div id="experience" className="text-center px-0 lg:px-5 py-10">
               <p
                 className="
               text-3xl font-bold"
@@ -145,76 +145,150 @@ export default function RootLayout({ ...props }) {
                 My work experience as a software engineer and working on
                 different companies and projects.
               </p>
-              <Timeline position="alternate">
-                {experiences.map((item, index) => {
-                  return (
-                    <TimelineItem key={index}>
-                      <TimelineOppositeContent
-                        color="text-white"
-                        className="mt-6"
-                      >
-                        {item.date}
-                      </TimelineOppositeContent>
-                      <TimelineSeparator>
-                        <TimelineDot variant="outlined" color="secondary" />
-                        {index !== experiences.length - 1 && (
-                          <TimelineConnector
-                            style={{ background: "#854CE6" }}
-                          />
-                        )}
-                      </TimelineSeparator>
-                      <TimelineContent sx={{ py: "28px", px: 2 }}>
-                        <div
-                          className="rounded-xl shadow-[0_0_12px_4px_rgba(133,_76,_230,_0.2)] hover:-translate-y-3 duration-200 hover:shadow-[0_15px_30px_rgba(133,_76,_230,_0.4)] border-[#844ce696] p-6 justify-between
-                      flex relative flex-col gap-4 overflow-hidden"
+              <div className="hidden lg:block">
+                <Timeline position="alternate">
+                  {experiences.map((item, index) => {
+                    return (
+                      <TimelineItem key={index}>
+                        <TimelineOppositeContent
+                          color="text-white"
+                          className="mt-6"
                         >
-                          <div className="w-full flex gap-4 items-center">
-                            <img
-                              className="w-12 h-12 p-1 bg-white rounded-lg "
-                              src={item?.img}
+                          {item.date}
+                        </TimelineOppositeContent>
+                        <TimelineSeparator>
+                          <TimelineDot variant="outlined" color="secondary" />
+                          {index !== experiences.length - 1 && (
+                            <TimelineConnector
+                              style={{ background: "#854CE6" }}
                             />
-                            <div className="flex flex-col items-start">
-                              <p className="text-lg font-semibold text-[#f2f3f499]">
-                                {item?.role}
-                              </p>
-                              <p className="text-sm font-medium text-[#b1b2b399]">
-                                {item.company}
-                              </p>
-                              <p className="text-xs text-[#b1b2b380]">
-                                {item?.date}
-                              </p>
-                            </div>
-                          </div>
-                          {item?.desc && (
-                            <span className="text-[#f2f3f499] text-left">
-                              {item?.desc}
-                            </span>
                           )}
-                          {item?.skills && (
-                            <>
-                              <div className="flex gap-2">
-                                <b className="text-[#f2f3f499]">Skills:</b>
-                                <div className="flex flex-wrap gap-2">
-                                  {item?.skills?.map((skill, index) => {
-                                    return (
-                                      <p
-                                        key={index}
-                                        className="text-[#f2f3f499]"
-                                      >
-                                        • {skill}
-                                      </p>
-                                    );
-                                  })}
-                                </div>
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: "28px", px: 2 }}>
+                          <div
+                            className="rounded-xl shadow-[0_0_12px_4px_rgba(133,_76,_230,_0.2)] hover:-translate-y-3 duration-200 hover:shadow-[0_15px_30px_rgba(133,_76,_230,_0.4)] border-[#844ce696] p-6 justify-between
+                      flex relative flex-col gap-4 overflow-hidden"
+                          >
+                            <div className="w-full flex gap-4 items-center">
+                              <img
+                                className="w-12 h-12 p-1 bg-white rounded-lg "
+                                src={item?.img}
+                              />
+                              <div className="flex flex-col items-start">
+                                <p className="text-lg font-semibold text-[#f2f3f499]">
+                                  {item?.role}
+                                </p>
+                                <p className="text-sm font-medium text-[#b1b2b399]">
+                                  {item.company}
+                                </p>
+                                <p className="text-xs text-[#b1b2b380]">
+                                  {item?.date}
+                                </p>
                               </div>
-                            </>
+                            </div>
+                            {item?.desc && (
+                              <span className="text-[#f2f3f499] text-left">
+                                {item?.desc}
+                              </span>
+                            )}
+                            {item?.skills && (
+                              <>
+                                <div className="flex gap-2">
+                                  <b className="text-[#f2f3f499]">Skills:</b>
+                                  <div className="flex flex-wrap gap-2">
+                                    {item?.skills?.map((skill, index) => {
+                                      return (
+                                        <p
+                                          key={index}
+                                          className="text-[#f2f3f499]"
+                                        >
+                                          • {skill}
+                                        </p>
+                                      );
+                                    })}
+                                  </div>
+                                </div>
+                              </>
+                            )}
+                          </div>
+                        </TimelineContent>
+                      </TimelineItem>
+                    );
+                  })}
+                </Timeline>
+              </div>
+              <div className="lg:hidden block">
+                <Timeline>
+                  {experiences.map((item, index) => {
+                    return (
+                      <TimelineItem key={index}>
+                        <TimelineOppositeContent
+                          color="text-white"
+                          className="hidden"
+                        >
+                          {item.date}
+                        </TimelineOppositeContent>
+                        <TimelineSeparator className="p-0">
+                          <TimelineDot variant="outlined" color="secondary" />
+                          {index !== experiences.length - 1 && (
+                            <TimelineConnector
+                              style={{ background: "#854CE6" }}
+                            />
                           )}
-                        </div>
-                      </TimelineContent>
-                    </TimelineItem>
-                  );
-                })}
-              </Timeline>
+                        </TimelineSeparator>
+                        <TimelineContent sx={{ py: "28px", px: 2 }}>
+                          <div
+                            className="rounded-xl shadow-[0_0_12px_4px_rgba(133,_76,_230,_0.2)] hover:-translate-y-3 duration-200 hover:shadow-[0_15px_30px_rgba(133,_76,_230,_0.4)] border-[#844ce696] p-6 justify-between
+                      flex relative flex-col gap-4 overflow-hidden"
+                          >
+                            <div className="w-full flex gap-4 items-center">
+                              <img
+                                className="w-12 h-12 p-1 bg-white rounded-lg "
+                                src={item?.img}
+                              />
+                              <div className="flex flex-col items-start">
+                                <p className="text-lg font-semibold text-[#f2f3f499]">
+                                  {item?.role}
+                                </p>
+                                <p className="text-sm font-medium text-[#b1b2b399]">
+                                  {item.company}
+                                </p>
+                                <p className="text-xs text-[#b1b2b380]">
+                                  {item?.date}
+                                </p>
+                              </div>
+                            </div>
+                            {item?.desc && (
+                              <span className="text-[#f2f3f499] text-left">
+                                {item?.desc}
+                              </span>
+                            )}
+                            {item?.skills && (
+                              <>
+                                <div className="flex gap-2">
+                                  <b className="text-[#f2f3f499]">Skills:</b>
+                                  <div className="flex flex-wrap gap-2">
+                                    {item?.skills?.map((skill, index) => {
+                                      return (
+                                        <p
+                                          key={index}
+                                          className="text-[#f2f3f499]"
+                                        >
+                                          • {skill}
+                                        </p>
+                                      );
+                                    })}
+                                  </div>
+                                </div>
+                              </>
+                            )}
+                          </div>
+                        </TimelineContent>
+                      </TimelineItem>
+                    );
+                  })}
+                </Timeline>
+              </div>
             </div>
             <div id="project" className="text-center px-5 py-10">
               <p
@@ -235,7 +309,7 @@ export default function RootLayout({ ...props }) {
                   return (
                     <div
                       key={index}
-                      className="group w-[30%] cursor-pointer rounded-lg shadow-[0_0_12px_4px_rgba(133,_76,_230,_0.2)] overflow-hidden p-6 flex flex-wrap gap-8 hover:-translate-y-3 brightness-110 duration-200 hover:shadow-[0_15px_30px_rgba(133,_76,_230,_0.4)]"
+                      className="group w-full md:w-[45%] lg:w-[45%] xl:w-[30%] cursor-pointer rounded-lg shadow-[0_0_12px_4px_rgba(133,_76,_230,_0.2)] overflow-hidden p-6 flex flex-wrap gap-8 hover:-translate-y-3 brightness-110 duration-200 hover:shadow-[0_15px_30px_rgba(133,_76,_230,_0.4)]"
                     >
                       <img
                         src={item?.image}
