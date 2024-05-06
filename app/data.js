eb6d25135e32eba705be887901541316b2ce48db
You <letrandanglong.0605@gmail.com>
2024-05-06 11:11:10 +0700

    update v2 06/05/.2024

--- app/data.js
+++ app/data.js
@@ -69,6 +69,22 @@ export const Bio = {
       ),
       url: "https://github.com/LongLee0605?tab=repositories",
     },
+    {
+      img: (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="44"
+          height="44"
+          viewBox="0 0 29 29"
+          id="user"
+        >
+          <path d="M14.5 2A12.514 12.514 0 0 0 2 14.5 12.521 12.521 0 0 0 14.5 27a12.5 12.5 0 0 0 0-25Zm7.603 19.713a8.48 8.48 0 0 0-15.199.008A10.367 10.367 0 0 1 4 14.5a10.5 10.5 0 0 1 21 0 10.368 10.368 0 0 1-2.897 7.213ZM14.5 7a4.5 4.5 0 1 0 4.5 4.5A4.5 4.5 0 0 0 14.5 7Z"></path>
+        </svg>
+      ),
+      url: "https://www.topcv.vn/xem-cv/AwEAVVMCB1kABQcBXQEDAABVBgYOU1RWCFZTDgcced",
+    },
   ],
 };
 // skill
